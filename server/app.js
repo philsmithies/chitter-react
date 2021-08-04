66fe93d64af34804973264949b3047cb61ae951e
Phil Smithies <philsmithies@gmail.com>
2021-08-04 02:12:08 +0200

    passport auth

--- server/app.js
+++ server/app.js
@@ -6,6 +6,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const Tweet = require('./models/tweet')
 const User = require('./models/user')
+const session = require("express-session");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
 require('dotenv').config();
 
 var indexRouter = require('./routes/index');
@@ -18,6 +21,40 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// passport
+
+passport.use(
+  new LocalStrategy((username, password, done) => {
+    User.findOne({ username: username }, (err, user) => {
+      if (err) { 
+        return done(err);
+      }
+      if (!user) {
+        return done(null, false, { message: "Incorrect username" });
+      }
+      if (user.password !== password) {
+        return done(null, false, { message: "Incorrect password" });
+      }
+      return done(null, user);
+    });
+  })
+);
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+
+app.use(session({ secret: "cats", resave: false, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // connect to mongodb
 const PORT = process.env.PORT || 3001 
@@ -40,7 +77,7 @@ app.get('/', function (req, res) {
   Tweet.find()
   .sort({ createdAt: -1 })
   .then((result) => {
-    res.render("index", { title: "All Tweets", tweets: result});
+    res.render("index", { title: "All Tweets", tweets: result, user: req.user});
   })
   .catch((err) => {
     console.log(err)
@@ -65,23 +102,39 @@ app.post("/new", (req, res) => {
 });
 
 // sign up
-app.get('/signup', function (req, res) {
-    res.render("signup", { title: 'Sign Up'});
-})
 
-app.post("/signup", (req, res) => {
-  const user = new User(req.body)
-  console.log(req.body)
-
-  user.save()
-  .then(result => {
-    res.redirect('/')
-  })
-  .catch((err) => {
-    console.log(err)
-  })
+app.get("/signup", (req, res) => {
+  res.render("signup", { title: "New" });
 });
 
+app.post("/signup", (req, res, next) => {
+  const user = new User({
+    username: req.body.username,
+    password: req.body.password,
+    email: req.body.email
+  }).save(err => {
+    if (err) { 
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
+
+// log in
+
+app.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/"
+  })
+);
+
+// log out 
+app.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/");
+});
 
 // app.use('/users', usersRouter);
 
