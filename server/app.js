da4ec58d48fb3f806a075e8c6c1eccfa829c45e0
Phil Smithies <philsmithies@gmail.com>
2021-08-06 23:48:28 +0200

    skeleton files

--- server/app.js
+++ server/app.js
@@ -129,18 +129,21 @@ app.get("/signup", (req, res) => {
 });
 
 app.post("/signup", async (req, res, next) => {
+  User.findOne({ username: req.body.username }, async (err, doc) => {
+    if (err) throw err;
+    if (doc) res.send("User Already Exists");
+    if (!doc) {
   const hashedPassword = await bcrypt.hash(req.body.password, 10);
-  const user = new User({
+  const newUser = new User({
     username: req.body.username,
     password: hashedPassword,
     email: req.body.email
-  }).save(err => {
-    if (err) { 
-      return next(err);
-    }
-
-    res.redirect("/");
-  });
+  })
+  await newUser.save();
+  res.send("User Created");
+  res.redirect("/");
+}
+});
 });
 
 // log in
--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
+import Index from './pages';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>chitter</h2>
+      <Router>
+      <Switch>
+        <Route path="/" exact component={Index}  />
+      </Switch>
+      </Router>
     </div>
   );
 }
