60dcef924793347f3f7400f5fcbc2c9533e52c8c
Phil Smithies <philsmithies@gmail.com>
2021-08-09 00:45:59 +0200

    date-fns formatting

--- src/components/Tweet/index.js
+++ src/components/Tweet/index.js
@@ -1,37 +1,41 @@
-import './index.css'
-import Feather from './feather.png'
-import RepeatIcon from '@material-ui/icons/Repeat';
-import ChatBubbleOutlineIcon from '@material-ui/icons/ChatBubbleOutline';
-import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder';
-import LinkIcon from '@material-ui/icons/Link';
-import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
-import { format } from 'date-fns'
+import "./index.css";
+import Feather from "./feather.png";
+import RepeatIcon from "@material-ui/icons/Repeat";
+import ChatBubbleOutlineIcon from "@material-ui/icons/ChatBubbleOutline";
+import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
+import LinkIcon from "@material-ui/icons/Link";
+import MoreHorizIcon from "@material-ui/icons/MoreHoriz";
+import { format } from "date-fns";
 
 export default function Tweet(props) {
-  return(
+  const formatDate = (date) => {
+    return format(new Date(date), "E MMM dd yyyy");
+  };
+
+  return (
     <div>
       <div className="tweet_wrapper">
         <div className="profile_photo">
-          <img src={Feather} alt="new user"/>
+          <img src={Feather} alt="new user" />
         </div>
         <div className="tweet_content">
           <div className="username">
-            <strong>{props.username}</strong> <small>@{props.username} {props.createdAt}</small>
+            <strong>{props.username}</strong> 
+            <small className="usernameText">@{props.username}</small>
+            <small className="dateText">{formatDate(props.createdAt)}</small>
             {/* <div className="threeDots">
               <MoreHorizIcon/>
             </div> */}
           </div>
-          <div className="body">
-            {props.tweet}
-          </div>
+          <div className="body">{props.tweet}</div>
           <div className="tweet_functions">
-            <ChatBubbleOutlineIcon className="chatBubble"/>
-            <RepeatIcon className="retweet"/>
-            <FavoriteBorderIcon className="heart"/>
-            <LinkIcon className="link"/>
+            <ChatBubbleOutlineIcon className="chatBubble" />
+            <RepeatIcon className="retweet" />
+            <FavoriteBorderIcon className="heart" />
+            <LinkIcon className="link" />
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
