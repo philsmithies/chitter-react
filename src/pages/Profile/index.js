eb13afad1957c053dd0b9fff95a0d9c8fe8272ac
Phil Smithies <philsmithies@gmail.com>
2021-08-13 04:26:24 +0200

    profile fix

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -12,73 +12,66 @@ export default function Profile() {
   const [tweets, setTweets] = useState();
   const { userId } = useParams();
 
+  const getProfileData = function (userId) {
+    if (!userId) {
+      axios({
+        method: "GET",
+        withCredentials: true,
+        url: "http://localhost:3001/profile",
+      }).then((res) => {
+        console.log(res.data);
+        setData(res.data);
+      });
+    } else {
+      axios({
+        method: "GET",
+        withCredentials: true,
+        url: "http://localhost:3001/profile/" + userId,
+      }).then((res) => {
+        setData(res.data);
+      });
+    }
+  };
+
+  const getTweets = async () => {
+    try {
+      await axios
+        .get("http://localhost:3001/users/" + userId + "/tweets/")
+        .then((response) => {
+          setTweets(response.data);
+        });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   useEffect(() => {
-    const getProfileData = function (userId) {
-      console.log("getProfileData");
-      if (!userId) {
-        axios({
-          method: "GET",
-          withCredentials: true,
-          url: "http://localhost:3001/profile",
-        }).then((res) => {
-          console.log(res.data);
-          setData(res.data);
-        });
-      } else {
-        axios({
-          method: "GET",
-          withCredentials: true,
-          url: "http://localhost:3001/profile/" + userId,
-        }).then((res) => {
-          setData(res.data);
-        });
-      }
-    };
-  
-    const getTweets = async () => {
-      try {
-        await axios
-          .get("http://localhost:3001/users/" + userId + "/tweets/")
-          .then((response) => {
-            setTweets(response.data);
-            console.log(tweets);
-          });
-      } catch (err) {
-        console.error(err);
-      }
-    };
-
-    getProfileData(userId)
-    getTweets()
-
-  }, [tweets, userId]);
+    getProfileData(userId);
+    getTweets();
+  }, []);
 
   return (
     <div className="profile_grid">
       {data ? (
-        <div>
-          <div className="profile_wrapper">
-            <ProfileWrapper
-              username={data.username}
-              fullName={data.fullName}
-              length={tweets ? tweets.length : ""}
-              cloudName="chitter"
-              publicId={data.publicId}
-            />
-          </div>
-          <div class="profile_content">
+        <div className="profile_wrapper">
+          <ProfileWrapper
+            username={data.username}
+            fullName={data.fullName}
+            length={tweets ? tweets.length : ""}
+            cloudName="chitter"
+            publicId={data.publicId}
+          />
+          <div className="profile_content">
             {tweets
               ? tweets.map((value, index) => (
-                  <div>
-                    <Tweet
-                      key={index}
-                      fullName={value.author.fullName}
-                      publicId={value.author ? value.author.publicId : ""}
-                      text={value.text}
-                      username={value.author ? value.author.username : ""}
-                      createdAt={value.createdAt}
-                    />
-                  </div>
+                  <Tweet
+                    key={index}
+                    fullName={value.author.fullName}
+                    publicId={value.author ? value.author.publicId : ""}
+                    text={value.text}
+                    username={value.author ? value.author.username : ""}
+                    createdAt={value.createdAt}
+                  />
                 ))
               : ""}
           </div>
