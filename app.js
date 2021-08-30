47b01cc49ea50f6529889397fa5b27966e826dc3
Phil Smithies <philsmithies@gmail.com>
2021-08-31 01:11:42 +0200

    jwt updated

--- app.js
+++ app.js
@@ -44,6 +44,7 @@ app.use(function (req, res, next) {
 
   // Website you wish to allow to connect
   res.setHeader('Access-Control-Allow-Origin', 'https://gracious-volhard-935a72.netlify.app');
+    // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
