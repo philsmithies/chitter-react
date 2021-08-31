208d27ca7d4580492c236651f378a256bad855ff
Phil Smithies <philsmithies@gmail.com>
2021-08-31 02:08:09 +0200

    jwt updated;

--- controllers/authController.js
+++ controllers/authController.js
@@ -58,7 +58,7 @@ module.exports.login_post = async (req, res) => {
   try {
     const user = await User.login(username, password);
     const token = createToken(user._id);
-    res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
+    res.cookie('jwt', token, { maxAge: maxAge * 1000 });
     // res.status(200).json({user, auth: true});
     res.status(200).json({ user: user._id, auth: true });
     // res.send("Successfully Authenticated")
